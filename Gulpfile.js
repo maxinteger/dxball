4680dadccfbfb130651b85f0fd18dcf0eee56d55
Vadasz Laszlo <maxinteger@gmail.com>
2014-09-29 18:01:07 +0200

    init setup

---
+++ Gulpfile.js
@@ -0,0 +1,91 @@
+(function(){
+    'use strict';
+
+    /* global require: false */
+    var gulp = require('gulp'),
+        gutil = require('gulp-util'),
+        clean = require('gulp-clean'),
+        notify = require('gulp-notify'),
+        es = require('event-stream'),
+    //plumber = require('gulp-plumber'),
+        changed = require('gulp-changed'),
+        traceur = require('gulp-traceur'),
+        connect = require('gulp-connect'),
+        symlink = require('gulp-symlink'),
+        less = require('gulp-less');
+
+
+    var appDir =        '',
+        distDir =       appDir + 'dist/',
+        srcDir =        appDir + 'src/',
+        testDir =       appDir + 'test/',
+        JSFiles =       srcDir + 'js/**/*.js',
+        JSTestFiles =   testDir + 'unit/**/*.js',
+        lessFiles =     srcDir + 'less/**/*.less',
+        HTMLFiles =     srcDir + '**/*.html',
+        assetFiles =    appDir + 'assets',
+        bowerDeps =     appDir + 'bower_components';
+
+    gulp.task('clean-dist', function () {
+        return gulp.src(distDir + '*', {read: false}).pipe(clean());
+    });
+
+    gulp.task('dev-js', function () {
+        return gulp.src(JSFiles)
+            .pipe(changed(distDir + 'js/'))
+            .pipe(traceur({
+                //experimental: true,
+                //sourceMap: false,
+                modules: 'amd'
+            })
+                .on('error', gutil.log)
+                .on("error", notify.onError(function (error) {
+                    return "Traceur Error: " + error.message;
+                }))
+        ).pipe(gulp.dest(distDir + 'js/'))
+            .pipe(connect.reload());
+    });
+
+    /**
+     * HTML fájlok újratöltése
+     */
+    gulp.task('dev-html', function () {
+        return  gulp.src(HTMLFiles)
+            .pipe(changed(distDir))
+            .pipe(gulp.dest(distDir));
+    });
+
+
+    gulp.task('dev-assets', function(){
+        return es.concat(
+            gulp.src(assetFiles).pipe(symlink(distDir)),
+            gulp.src(bowerDeps).pipe(symlink(distDir))
+        );
+    });
+
+
+    /**
+     * Less fordítás
+     */
+    gulp.task('dev-less', function () {
+        return gulp.src([srcDir + 'less/main.less'])
+            .pipe(changed(distDir + 'css/'))
+            .pipe(less()
+                .on('error', gutil.log)
+                .on("error", notify.onError(function (error) {
+                    return "Less Error: " + error.message;
+                }))
+        )
+            .pipe(gulp.dest(distDir + 'css/'))
+            .pipe(connect.reload());
+    });
+
+
+    gulp.task('dev', ['clean-dist'], function () {
+        runSeq('dev-assets', 'dev-less', 'dev-js', 'dev-html', function(){
+            gulp.watch(JSFiles, ['dev-js']);
+            gulp.watch(HTMLFiles, ['dev-html']);
+            gulp.watch(lessFiles, ['dev-less']);
+        });
+    });
+})();
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by vadasz on 2014.09.29..
+ */
+
+console.log('Hello world');
